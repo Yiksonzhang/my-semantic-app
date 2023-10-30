b79f1bafcc2bf084679e2468b66e813957ccce2b
Yiksonzhang <yiksonzhang@gmail.com>
2023-10-30 18:01:38 +0800

    It's my first time to offer git message here

---
+++ src/footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import App from './App';
+
+function footer(){
+    returen (
+        <footer>
+            {/* 具体的footer内容放在这里 */}
+        </footer>
+
+    )
+}
+
+export default footer 
---
+++ src/nav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Nav(){
+    return(
+        <nav>
+            {/* 具体的导航内容放在这里*/}
+        </nav>
+    )
+}
+
+export default Nav;
